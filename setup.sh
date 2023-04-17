858915ab59962051b58edd1332299d34f4522279
Aleksey Lebedev <aleksey.lebedev@veeam.com>
2023-04-17 23:23:44 +0200

    make setup.sh executable and fix links

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
--- Link alacritty config
-ln -s $HOME/.dotfiles/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+# Link alacritty config
+ln -s $HOME/.dotfiles/alacritty.yml $HOME/.config/alacritty/alacritty.yml 
 
--- Link tmux config
-ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+# Link tmux config
+ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf 
