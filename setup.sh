c50f4942ae1369fc2ba30773fecae2663620235a
Aleksey Lebedev <aleksey.lebedev@veeam.com>
2023-04-17 23:16:01 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+-- Link alacritty config
+ln -s $HOME/.dotfiles/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+
+-- Link tmux config
+ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
