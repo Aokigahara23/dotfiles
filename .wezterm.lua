3bb79abed566451c44fddf9fb5adf6061a7e4605
forest23 <aokigahara23@gmail.com>
2024-06-08 14:06:34 +0200

    update wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,10 +9,39 @@ config.font = wezterm.font({
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
 
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 config.font_size = 14.0
 config.color_scheme = "Catppuccin Mocha"
+config.window_decorations = "NONE"
 config.enable_tab_bar = true
+config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+config.tab_max_width = 25
+config.show_tab_index_in_tab_bar = true
+config.inactive_pane_hsb = {
+	saturation = 0.8,
+	brightness = 0.80,
+}
+config.window_close_confirmation = "NeverPrompt"
+config.colors = {
+	tab_bar = {
+		background = "#1e1e2e",
+		active_tab = {
+			bg_color = "#cba6f7",
+			fg_color = "#1e1e2e",
+		},
+		inactive_tab = {
+			bg_color = "#313244",
+			fg_color = "#6c7086",
+		},
+	},
+}
 
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
@@ -48,22 +77,22 @@ config.keys = {
 	},
 	{
 		key = "LeftArrow",
-		mods = "ALT",
+		mods = "CTRL|SHIFT",
 		action = wezterm.action.ActivatePaneDirection("Left"),
 	},
 	{
 		key = "RightArrow",
-		mods = "ALT",
+		mods = "CTRL|SHIFT",
 		action = wezterm.action.ActivatePaneDirection("Right"),
 	},
 	{
 		key = "UpArrow",
-		mods = "ALT",
+		mods = "CTRL|SHIFT",
 		action = wezterm.action.ActivatePaneDirection("Up"),
 	},
 	{
 		key = "DownArrow",
-		mods = "ALT",
+		mods = "CTRL|SHIFT",
 		action = wezterm.action.ActivatePaneDirection("Down"),
 	},
 	{
