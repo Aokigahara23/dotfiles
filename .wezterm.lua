9297696ea8f4bbe641711629040613f683fcab4f
forest23 <aokigahara23@gmail.com>
2024-06-10 11:13:51 +0200

    upd

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font({
-	family = "JetBrainsMono Nerd Font",
+	family = "FiraCode Nerd Font",
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
 
