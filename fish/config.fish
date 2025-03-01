771069a979f42bdc86ce63397ef75967d71ac6fb
Aleksey Lebedev <aleksey.lebedev@veeam.com>
2025-03-01 15:25:27 +0100

    update

--- fish/config.fish
+++ fish/config.fish
@@ -2,23 +2,28 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# ALIASES #
+### BINDS ###
+fish_default_key_bindings
+
+### ALIASES ###
 alias ls "exa"
 alias ll "exa -lah"
 alias tree "exa --tree"
-# ENV VARS #
+
+### ENV VARS ###
 set -Ux FZF_DEFAULT_OPTS "\
+--tmux center \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --color=selected-bg:#45475a \
 --multi"
 
-# ADDONS #
+### ADDONS ###
 starship init fish | source
+fzf --fish | source
 
-fish_vi_key_bindings
+### FUNCTIONS ###
 set fish_greeting
 
 set -x PATH ~/.cargo/bin $PATH
-set -x PATH ~/.config/emacs/bin $PATH
